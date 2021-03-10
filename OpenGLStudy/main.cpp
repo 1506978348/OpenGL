b8f291f2a292d63c134db52adc672227489b08b4
chenzhi <chenzhi@tinman.cn>
2021-03-10 17:20:16 +0800

    添加颜色分类

--- OpenGLStudy/main.cpp
+++ OpenGLStudy/main.cpp
@@ -4,6 +4,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <cmath>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -15,18 +16,20 @@ const unsigned int SCR_HEIGHT = 600;
 //顶点着色器代码
 const char *vertexShaderSource="#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
-"out vec4 vertexColor;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
 "gl_Position =vec4(aPos,1.0);\n"
-"vertexColor=vec4(0.5,0.0,0.0,1.0);\n"
+"ourColor=aColor;\n"
 "}\n";
 //片元着色器代码
 const char *fragmentShaderSource="#version 330 core\n"
 "out vec4 FragColor;"
-"in vec4 vertexColor;\n"
+"uniform vec4 unifColor;"
+"in vec3 ourColor;\n"
 "void main()\n"
-"{FragColor =vertexColor;}";
+"{FragColor =vec4(ourColor,1.0f);}";
 
 
 int main()
@@ -107,10 +110,11 @@ int main()
     glad_glDeleteShader(fragmentShader);
     
     float vertices[]={
-        0.5f,0.5f,0.0f, //右上角
-        0.5f,-0.5f,0.0f,//右下角
-        -0.5f,-0.5f,0.0f,//左下角
-        -0.5f,0.5f,0.0f,//左上角
+        //位置          //颜色
+        0.5f,0.5f,0.0f,   1.0f,0.0f,0.0f,  //右上角
+        0.5f,-0.5f,0.0f,  0.0f,1.0f,0.0f,//右下角
+        -0.5f,-0.5f,0.0f, 0.0f,0.0f,1.0f,//左下角
+        -0.5f,0.5f,0.0f,  1.0f,1.0f,0.0f//左上角
     };
     
     //opengl 右手坐标系
@@ -135,9 +139,12 @@ int main()
     glad_glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
     
     //设置顶点属性指针
-    glad_glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
+    glad_glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)0);
     glad_glEnableVertexAttribArray(0);
     
+    //颜色属性
+    glad_glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)(3* sizeof(float)));
+    glad_glEnableVertexAttribArray(1);
 //      note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glad_glBindBuffer(GL_ARRAY_BUFFER,0);
 //     You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
@@ -159,8 +166,14 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
-//       draw triangle
+//      激活着色器
         glad_glUseProgram(shaderPorgram);
+        
+//        //更新uniform  outColor颜色
+//        float timeValue=glfwGetTime();
+//        float greenValue=sin(timeValue)/2.0f+0.5f;
+//        int vertexColorLocation=glad_glGetUniformLocation(shaderPorgram,"unifColor");
+//        glad_glUniform4f(vertexColorLocation,0.0f,greenValue,0.0f,1.0f);
         
         glad_glBindVertexArray(VAO);//seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 //        glad_glDrawArrays(GL_TRIANGLES,0,3);
