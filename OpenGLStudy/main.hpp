8fad462919af838cf6a4938d4e6a87f0d62adf36
chenzhi <chenzhi@tinman.cn>
2021-03-10 11:36:02 +0800

    1.0

---
+++ OpenGLStudy/main.hpp
@@ -0,0 +1,14 @@
+//
+//  main.hpp
+//  OpenGLStudy
+//
+//  Created by 陈志 on 2021/2/20.
+//  Copyright © 2021 陈志. All rights reserved.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
